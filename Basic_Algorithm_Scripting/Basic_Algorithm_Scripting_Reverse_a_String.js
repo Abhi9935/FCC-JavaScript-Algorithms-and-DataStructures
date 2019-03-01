f5f38d2044b82e52830a91c34420eb84b61153d0
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-03-01 21:14:27 +0530

    Reverse a String

--- Basic_Algorithm_Scripting/Basic_Algorithm_Scripting_Reverse_a_String.js
+++ Basic_Algorithm_Scripting/Basic_Algorithm_Scripting_Reverse_a_String.js
@@ -22,3 +22,15 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+//----------------------------------------------------
+//--------------------- or ---------------------------
+//----------------------------------------------------
+function reverseString(str) {
+  let newStr="";
+  for (var i = str.length - 1; i >= 0; i--) { 
+    newStr += str[i]; 
+  }
+  return newStr;
+}
+
+reverseString("hello"); 
