5d5a2e0f459e9960548688a67bc889392f13193a
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-03-02 22:55:19 +0530

    Boo who

--- Basic_Algorithm_Scripting/Basic_Algorithm_Scripting_Boo_who.js
+++ Basic_Algorithm_Scripting/Basic_Algorithm_Scripting_Boo_who.js
@@ -32,3 +32,16 @@ function booWho(bool) {
   return (typeof bool === 'boolean');
 }
 booWho(null);
+//----------------------------------------------------
+//--------------------- OR ---------------------------
+//----------------------------------------------------
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+booWho(null);
