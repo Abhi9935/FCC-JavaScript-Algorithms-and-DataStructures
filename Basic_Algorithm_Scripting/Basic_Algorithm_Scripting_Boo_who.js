dd50b6177173bbe2879a8ecf4cf05f88692c3702
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-03-02 22:52:26 +0530

    Boo who

---
+++ Basic_Algorithm_Scripting/Basic_Algorithm_Scripting_Boo_who.js
@@ -0,0 +1,34 @@
+/*
+Basic Algorithm Scripting: Boo who
+
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Test Cases:-
+Passed
+booWho(true) should return true.
+Passed
+booWho(false) should return true.
+Passed
+booWho([1, 2, 3]) should return false.
+Passed
+booWho([].slice) should return false.
+Passed
+booWho({ "a": 1 }) should return false.
+Passed
+booWho(1) should return false.
+Passed
+booWho(NaN) should return false.
+Passed
+booWho("a") should return false.
+Passed
+booWho("true") should return false.
+Passed
+booWho("false") should return false.
+*/
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return (typeof bool === 'boolean');
+}
+booWho(null);
