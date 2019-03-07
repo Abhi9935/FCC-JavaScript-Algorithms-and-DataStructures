c9e53a4ee9d08b250a54bc5c4a483ce11e1d59de
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-03-07 21:22:52 +0530

    Accessing Object Properties with Bracket Notation

---
+++ Basic JavaScript/Basic_JavaScript_Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,45 @@
+/*
+Basic JavaScript: Accessing Object Properties with Bracket Notation
+
+The second way to access the properties of an object is bracket notation ([]). 
+If the property of the object you are trying to access has a space in its name, 
+you will need to use bracket notation.
+However, you can still use bracket notation on object properties without spaces.
+Here is a sample of using bracket notation to read an object's property:
+```
+var myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+myObj["Space Name"]; // Kirk
+myObj['More Space']; // Spock
+myObj["NoSpace"]; // USS Enterprise
+```
+Note that property names with spaces in them must be in quotes (single or double).
+Read the values of the properties "an entree" and "the drink" of testObj using bracket 
+notation and assign them to entreeValue and drinkValue respectively.
+
+Test Cases:-
+Passed
+entreeValue should be a string
+Passed
+The value of entreeValue should be "hamburger"
+Passed
+drinkValue should be a string
+Passed
+The value of drinkValue should be "water"
+Passed
+You should use bracket notation twice
+*/
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
