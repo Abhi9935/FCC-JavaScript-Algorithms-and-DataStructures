1c88f9b2785e2d760d0c760854b1cee81cc41395
Abhinav <kushwahaabinav96@gmail.com>
2019-03-12 13:01:17 +0530

    Testing Objects for Properties

--- Basic JavaScript/Basic_JavaScript_Testing_Objects_for_Properties.js
+++ Basic JavaScript/Basic_JavaScript_Testing_Objects_for_Properties.js
@@ -4,7 +4,6 @@ Basic JavaScript: Testing Objects for Properties
 Sometimes it is useful to check if the property of a given object exists or not. We can use the 
 .hasOwnProperty(propname) method of objects to determine if that object has the given property name. 
 .hasOwnProperty() returns true or false if the property is found or not.
-
 Example
 ```
 var myObj = {
