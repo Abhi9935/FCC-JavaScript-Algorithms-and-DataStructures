29d5f87dd43190714a6bc5a631025254773e008e
Abhinav <kushwahaabinav96@gmail.com>
2019-04-06 23:27:29 +0530

    Prevent Object Mutation

--- ES6/ES6_Prevent_Object_Mutation.js
+++ ES6/ES6_Prevent_Object_Mutation.js
@@ -1,8 +1,10 @@
 /*
 ES6: Prevent Object Mutation
-As seen in the previous challenge, const declaration alone doesn't really protect your data from mutation. To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation.
 
-Once the object is frozen, you can no longer add, update, or delete properties from it. Any attempt at changing the object will be rejected without an error.
+As seen in the previous challenge, const declaration alone doesn't really protect your data from mutation. 
+To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation.
+Once the object is frozen, you can no longer add, update, or delete properties from it. Any attempt at changing the object will be 
+rejected without an error.
 ```
 let obj = {
   name:"FreeCodeCamp",
@@ -14,7 +16,8 @@ obj.newProp = "Test"; // will be ignored. Mutation not allowed
 console.log(obj); 
 // { name: "FreeCodeCamp", review:"Awesome"}
 ```
-In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able alter the value of PI, add, or delete properties .
+In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
+You need to freeze the MATH_CONSTANTS object so that no one is able alter the value of PI, add, or delete properties .
 
 Test Cases:-
 Passed
