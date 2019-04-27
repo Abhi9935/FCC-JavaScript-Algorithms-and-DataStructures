82fe9c06b2d9882daf9caad4e0edc11e1acb169a
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-04-27 22:52:12 +0530

    Catch Use of Assignment Operator Instead of Equality Operator

---
+++ Debugging/Debugging_Catch_Use_of_Assignment_Operator_Instead_of_Equality_Operator.js
@@ -0,0 +1,46 @@
+/*
+Debugging: Catch Use of Assignment Operator Instead of Equality Operator
+
+Branching programs, i.e. ones that do different things if certain conditions are met, 
+rely on if, else if, and else statements in JavaScript. The condition sometimes takes the form of 
+testing whether a result is equal to a value.
+This logic is spoken (in English, at least) as "if x equals y, then ..." which can literally 
+translate into code using the =, or assignment operator. This leads to unexpected control flow in your program.
+
+As covered in previous challenges, the assignment operator (=) in JavaScript assigns a value to 
+a variable name. And the == and === operators check for equality (the triple === tests for strict equality, 
+meaning both value and type are the same).
+
+The code below assigns x to be 2, which evaluates as true. Almost every value on its own in JavaScript 
+evaluates to true, except what are known as 
+the "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null.
+```
+let x = 1;
+let y = 2;
+if (x = y) {
+  // this code block will run for any value of y (unless y were originally set as a falsy)
+} else {
+  // this code block is what should run (but won't) in this example
+}
+```
+
+Fix the condition so the program runs the right branch, and the appropriate value is assigned to result.
+
+Test Cases:-
+Passed
+Your code should fix the condition so it checks for equality, instead of using assignment.
+Passed
+The condition can use either == or === to test for equality.
+*/
+//-----------------------------------------------------------
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
