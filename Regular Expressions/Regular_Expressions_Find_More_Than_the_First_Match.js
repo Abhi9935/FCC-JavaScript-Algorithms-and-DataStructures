592d53c2f15dc3203e6fbc505929e6a46f04bc03
Abhi9935 <38867779+Abhi9935@users.noreply.github.com>
2019-05-09 22:40:19 +0530

    Find More Than the First Match

---
+++ Regular Expressions/Regular_Expressions_Find_More_Than_the_First_Match.js
@@ -0,0 +1,35 @@
+/*
+Regular Expressions: Find More Than the First Match
+
+So far, you have only been able to extract or search a pattern once.
+```
+let testStr = "Repeat, Repeat, Repeat";
+let ourRegex = /Repeat/;
+testStr.match(ourRegex);
+// Returns ["Repeat"]
+```
+To search or extract a pattern more than once, you can use the g flag.
+```
+let repeatRegex = /Repeat/g;
+testStr.match(repeatRegex);
+// Returns ["Repeat", "Repeat", "Repeat"]
+```
+Using the regex starRegex, find and extract both "Twinkle" words from the string twinkleStar.
+
+Note
+You can have multiple flags on your regex like /search/gi
+
+Test Cases:-
+Passed
+Your regex starRegex should use the global flag g
+Passed
+Your regex starRegex should use the case insensitive flag i
+Passed
+Your match should match both occurrences of the word "Twinkle"
+Passed
+Your match result should have two elements in it.
+*/
+//--------------------------------------------------------------------
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
